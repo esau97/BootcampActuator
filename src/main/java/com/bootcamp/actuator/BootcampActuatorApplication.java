25a9af89d17db2c0250c099cb6543805fa115630
Esau <esauhp97@gmail.com>
2021-11-30 14:33:02 +0100

    First commit

---
+++ src/main/java/com/bootcamp/actuator/BootcampActuatorApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampActuatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampActuatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bootcamp/actuator/BootcampActuatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.actuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampActuatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
